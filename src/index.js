bcab81d39130c090540bc66d56d84d4a604d9810
jona.e <inktechprintingservices@gmail.com>
2025-06-28 16:31:42 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const isLocalhost =
 const ID = isLocalhost ? LOCAL_ID : PRODUCTION_ID;
 const REDIRECT = isLocalhost
   ? "http://localhost:3000"
-  : "https://studio-app.inktechprinting.services";
+  : "https://ink-tech-studio.vercel.app/";
 
 function Fallback({ error, resetErrorBoundary }) {
   // Call resetErrorBoundary() to reset the error boundary and retry the render.
